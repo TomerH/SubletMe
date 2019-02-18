48006a8d47e47d61d4146f09b18130fecbbf2834
th-prod <47518391+th-prod@users.noreply.github.com>
2019-02-18 15:42:36 +0200

    Initialize project

---
+++ SubletMe/SubletMe.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SubletMe.WebUI.MvcApplication" Language="C#" %>
---
+++ SubletMe/SubletMe.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SubletMe.WebUI.Startup))]
+namespace SubletMe.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
